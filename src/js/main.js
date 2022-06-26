d47898f4e7ef7ebe7d92e76026d20fa900f59ea7
Igor <guzeca2@mail.ru>
2022-06-27 02:06:36 +0300

    add js slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,34 @@
-// const sum = require("./module/sum.js");
-// console.log(sum(2, 10));
-$( function() {
-    
-    if (localStorage.getItem("myKey")) {
-        var stored_select = localStorage.getItem("myKey");
-        $('#' + stored_select).prop( "selected", true );
-        $('.' + stored_select).show();
-        } else {
-    $('.Lekki').show();
-    }
-    });
-     
-    $("#selectlocation").change(function(){
-        $('.select-menu-location').find('div').hide();
-        var selected = $('#selectlocation option:selected').attr('id');
-        localStorage.setItem("myKey", selected);
-        $('.' + selected).show();
-    });
-// Property =================================
-$( function() {
- 
-   if (localStorage.getItem("myKeys")) {
-       var stored_select = localStorage.getItem("myKeys");
-       $('#' + stored_select).prop( "selected", true );
-       $('.' + stored_select).show();
-       } else {
-   $('.House').show();
-   }
-   });
-    
-   $("#selectproperty").change(function(){
-       $('.select-menu-property').find('div').hide();
-       var selected = $('#selectproperty option:selected').attr('id');
-       localStorage.setItem("myKeys", selected);
-       $('.' + selected).show();
-   }); 
-//    price =========================
-$( function() {
- 
-   if (localStorage.getItem("myKeyq")) {
-       var stored_select = localStorage.getItem("myKeyq");
-       $('#' + stored_select).prop( "selected", true );
-       $('.' + stored_select).show();
-       } else {
-   $('.one').show();
-   }
-   });
-    
-   $("#selectprice").change(function(){
-       $('.select-menu-price').find('div').hide();
-       var selected = $('#selectprice option:selected').attr('id');
-       localStorage.setItem("myKeyq", selected);
-       $('.' + selected).show();
-   }); 
+let test = document.getElementsByClassName('slider-button');
+test[0].onclick = function () {
+
+    document.getElementById('one').classList.add('current');
+    document.getElementById('two').classList.remove('current');
+    document.getElementById('three').classList.remove('current');
+
+    document.getElementById('japan').classList.add('current-slide-left');
+    document.getElementById('japan').classList.remove('current-slide');
+    document.getElementById('japan').classList.remove('current-slide-right');
+
+}; 
+test[1].onclick = function () {
+
+    document.getElementById('one').classList.remove('current');
+    document.getElementById('two').classList.add('current');
+    document.getElementById('three').classList.remove('current');
+
+    document.getElementById('japan').classList.remove('current-slide-left');
+    document.getElementById('japan').classList.add('current-slide');
+    document.getElementById('japan').classList.remove('current-slide-right');
+
+}; 
+test[2].onclick = function () {
+
+    document.getElementById('one').classList.remove('current');
+    document.getElementById('two').classList.remove('current');
+    document.getElementById('three').classList.add('current');
+
+    document.getElementById('japan').classList.remove('current-slide-left');
+    document.getElementById('japan').classList.remove('current-slide');
+    document.getElementById('japan').classList.add('current-slide-right');
+
+};
