f854ff362f8639a306fc1979920d8c79f2fa0a65
Igor <guzeca2@mail.ru>
2022-06-27 15:16:21 +0300

    add new js

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,9 @@
-let test = document.getElementsByClassName('slider-button');
-test[0].onclick = function () {
+$('.sliders-control > button').click(function () {
+    const currentSlide = $(this).data('country');
 
-    document.getElementById('one').classList.add('current');
-    document.getElementById('two').classList.remove('current');
-    document.getElementById('three').classList.remove('current');
+    $('.sliders-control > button').not(this).removeClass('current');
+    $(this).addClass('current');
 
-    document.getElementById('japan').classList.add('current-slide-left');
-    document.getElementById('japan').classList.remove('current-slide');
-    document.getElementById('japan').classList.remove('current-slide-right');
-
-}; 
-test[1].onclick = function () {
-
-    document.getElementById('one').classList.remove('current');
-    document.getElementById('two').classList.add('current');
-    document.getElementById('three').classList.remove('current');
-
-    document.getElementById('japan').classList.remove('current-slide-left');
-    document.getElementById('japan').classList.add('current-slide');
-    document.getElementById('japan').classList.remove('current-slide-right');
-
-}; 
-test[2].onclick = function () {
-
-    document.getElementById('one').classList.remove('current');
-    document.getElementById('two').classList.remove('current');
-    document.getElementById('three').classList.add('current');
-
-    document.getElementById('japan').classList.remove('current-slide-left');
-    document.getElementById('japan').classList.remove('current-slide');
-    document.getElementById('japan').classList.add('current-slide-right');
-
-};
+    $('.destination-country').not(currentSlide).removeClass('current-slide');
+    $(currentSlide).addClass('current-slide');
+});
